1a1526d562be32aa75711779048b47126413d5bb
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-09 23:30:46 -0300

    user

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -48,7 +48,7 @@ class DBStorage:
                 key = cls.__class.__.__name__ + obj.id
                 new_dict[key] = obj
         else:
-            for _cls in ['State', 'City']:
+            for _cls in ['State', 'City', 'User']:
                 objs = self.__session.query(eval(_cls)).all()
                 for obj in objs:
                     key = _cls + "." + obj.id
