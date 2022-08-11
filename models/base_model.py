5ca641df3ac1acca7db3235e1d0236084b879699
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-10 23:25:00 -0300

    basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -18,7 +18,7 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs or getenv('HBNB_TYPE_STORAGE') == 'db':
+        if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
