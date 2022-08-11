dcdfdd984e5c92c26c70872fb27ea590f1e96f91
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-11 16:49:29 -0300

    amenities

--- models/amenity.py
+++ models/amenity.py
@@ -8,4 +8,4 @@ from models.place import place_amenity
 class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity)
+    place_amenities = relationship("Place",back_populates="amenities", secondary=place_amenity)
