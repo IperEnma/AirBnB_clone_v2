ff1a4a772c1306ff88b05b1134184ad888365261
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-11 11:16:09 -0300

    import

--- models/__init__.py
+++ models/__init__.py
@@ -3,6 +3,11 @@
 from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
 from os import getenv
+from models.state import State
+from models.city import City
+from models.user import User
+from models.place import Place
+from models.amenity import Amenity
 
 type_s = getenv('HBNB_TYPE_STORAGE')
 
