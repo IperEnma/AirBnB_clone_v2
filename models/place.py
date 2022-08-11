1d27e3b9e3cf2bf604f011f71564d16ce6eb1e91
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-10 23:20:33 -0300

    relation

--- models/place.py
+++ models/place.py
@@ -32,7 +32,7 @@ class Place(BaseModel, Base):
 
     if (getenv('HBNB_TYPE_STORAGE') == 'db'):
         reviews = relationship("Review", backref="place", cascade="all, delete")
-        amenities = relationship("Amenity", secondary=place_amenity, viewonly=False)
+        amenities = relationship("Amenity", secondary=place_amenity, viewonly=False, back_populates="place_amenities")
 
     if (getenv('HBNB_TYPE_STORAGE') != 'db'):
         @property
