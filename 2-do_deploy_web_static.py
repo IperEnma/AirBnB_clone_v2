586b818460572e1bee015f62e7cffc4b9ed358e8
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-25 15:48:24 -0300

    asd

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -38,21 +38,20 @@ def do_deploy(archive_path):
     if (upload.succeeded is not True):
         return False
     file = os.path.basename(archive_path)
-    with cd("/tmp"):
-        try:
-            file = file.split(".")
-            run("mkdir -p /data/web_static/releases/{}".format(file[0]))
-            run("sudo tar -xvf {}.{} -C /data/web_static/releases/{}".format(
-                file[0], file[1], file[0]))
-            run("sudo rm -f {}.{}".format(file[0], file[1]))
-            run("sudo rm -f /data/web_static/current")
-            run("sudo cp -r /data/web_static/releases/{}/web_static/*\
-                /data/web_static/releases/{}".format(file[0], file[0]))
-            run("sudo rm -rf /data/web_static/releases/{}/web_static".format(
+    try:
+        file = file.split(".")
+        run("mkdir -p /data/web_static/releases/{}".format(file[0]))
+        run("sudo tar -xvf {}.{} -C /data/web_static/releases/{}".format(
+            file[0], file[1], file[0]))
+        run("sudo rm -f /tmp/{}.{}".format(file[0], file[1]))
+        run("sudo rm -f /data/web_static/current")
+        run("sudo cp -r /data/web_static/releases/{}/web_static/*\
+            /data/web_static/releases/{}".format(file[0], file[0]))
+        run("sudo rm -rf /data/web_static/releases/{}/web_static".format(
                 file[0]))
-            run("sudo ln -s -f /data/web_static/releases/{}/\
-                /data/web_static/current".format(file[0]))
-        except Exception:
-            return False
+        run("sudo ln -s -f /data/web_static/releases/{}/\
+            /data/web_static/current".format(file[0]))
+    except Exception:
+        return False
     print("New version deployed!")
     return True
