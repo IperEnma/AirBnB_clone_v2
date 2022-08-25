22637808458517246ba0f1837b88e4ac20bcebe9
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-25 15:57:51 -0300

    <zf

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -39,18 +39,19 @@ def do_deploy(archive_path):
         return False
     file = os.path.basename(archive_path)
     try:
-        file = file.split(".")
-        run("mkdir -p /data/web_static/releases/{}".format(file[0]))
-        run("sudo tar -xvf /tmp/{}.{} -C /data/web_static/releases/{}".format(
-            file[0], file[1], file[0]))
-        run("sudo rm -f /tmp/{}.{}".format(file[0], file[1]))
-        run("sudo rm -f /data/web_static/current")
-        run("sudo cp -r /data/web_static/releases/{}/web_static/*\
-            /data/web_static/releases/{}".format(file[0], file[0]))
-        run("sudo rm -rf /data/web_static/releases/{}/web_static".format(
-                file[0]))
-        run("sudo ln -s -f /data/web_static/releases/{}/\
-            /data/web_static/current".format(file[0]))
+        with cd("/tmp"):
+            file = file.split(".")
+            run("mkdir -p /data/web_static/releases/{}".format(file[0]))
+            run("sudo tar -xzf {}.{} -C /data/web_static/releases/{}".format(
+                file[0], file[1], file[0]))
+            run("sudo rm -f /{}.{}".format(file[0], file[1]))
+            run("sudo rm -f /data/web_static/current")
+            run("sudo cp -r /data/web_static/releases/{}/web_static/*\
+                /data/web_static/releases/{}".format(file[0], file[0]))
+            run("sudo rm -rf /data/web_static/releases/{}/web_static".format(
+                    file[0]))
+            run("sudo ln -s -f /data/web_static/releases/{}/\
+                /data/web_static/current".format(file[0]))
     except Exception:
         return False
     print("New version deployed!")
