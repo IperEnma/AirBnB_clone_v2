73c109c292861dc06d41728cfb367e13a750091f
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-25 14:29:11 -0300

    pycode

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,12 +4,13 @@
 from fabric.api import local
 from datetime import datetime
 
+
 def do_pack():
     try:
         date = datetime.utcnow().strftime('%Y%m%d%H%M%S')
-        print("Packing web_static to versions/web_static_{}.tgz web_static".format(date))
         local("sudo mkdir -p versions")
-        local("sudo tar -cvzf versions/web_static_{}.tgz web_static".format(date))
+        local("sudo tar -cvzf versions/web_static_{}.tgz web_static".format(
+            date))
         return "versions/web_static_{}.tgz web_static".format(date)
     except Exception:
         return None
