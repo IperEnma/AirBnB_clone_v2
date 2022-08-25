8ed6d95acebb81c74c3e4e3e9718f3e247d7e0a5
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-25 20:21:47 -0300

    asdasd

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -27,30 +27,29 @@ def do_deploy(archive_path):
         return False
 
     file = os.path.basename(archive_path)
-    with cd("/tmp"):
-        try:
-            put(archive_path, "/tmp")
-            file = file.split(".")
-            run("mkdir -p /data/web_static/releases/{}".format(file[0]))
+    try:
+        put(archive_path, "/tmp")
+        file = file.split(".")
+        run("mkdir -p /data/web_static/releases/{}".format(file[0]))
 
-            run("tar -xzf {}.{} -C /data/web_static/releases".format(
-                file[0], file[1]))
+        run("tar -xzf /tmp/{}.{} -C /data/web_static/releases".format(
+            file[0], file[1]))
 
-            run("rm -rf {}.{}".format(file[0], file[1]))
+        run("rm -rf /tmp/{}.{}".format(file[0], file[1]))
 
-            source = "/data/web_static/releases/web_static "
-            dest = "/data/web_static/releases/{}".format(file[0])
-            run("cp -R " + source + dest)
+        source = "/data/web_static/releases/web_static "
+        dest = "/data/web_static/releases/{}".format(file[0])
+        run("cp -R " + source + dest)
 
-            run("rm -rf /data/web_static/releases/{}/web_static".format(
-                file[0]))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(
+            file[0]))
 
-            run("rm -f /data/web_static/current")
+        run("rm -f /data/web_static/current")
 
-            source = "/data/web_static/releases/{}/ ".format(file[0])
-            dest = "/data/web_static/current"
-            run("ln -s " + source + dest)
+        source = "/data/web_static/releases/{}/ ".format(file[0])
+        dest = "/data/web_static/current"
+        run("ln -s " + source + dest)
 
-        except Exception:
-            return False
+    except Exception:
+        return False
     return True
