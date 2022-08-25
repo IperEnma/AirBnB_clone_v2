4276edb9aa74c8a7f4a4ca9c7f2d596174bac568
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-25 14:51:30 -0300

    dodeploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -12,20 +12,6 @@ env.user = argv[7]
 env.key_filename = argv[5]
 
 
-def do_pack():
-    """function compress file"""
-    try:
-        date = datetime.utcnow().strftime('%Y%m%d%H%M%S')
-        print("Packing web_static to versions/web_static_{}.\
-                tgz web_static".format(date))
-        local("sudo mkdir -p versions")
-        local("sudo tar -cvzf versions/web_static_{}.tgz web_static".format(
-            date))
-        return "versions/web_static_{}.tgz web_static".format(date)
-    except Exception:
-        return None
-
-
 def do_deploy(archive_path):
     """function send file"""
     if archive_path is None:
@@ -33,14 +19,4 @@ def do_deploy(archive_path):
     upload = put(archive_path, "/tmp")
     file = os.path.basename(archive_path)
     print(file)
-    with cd("/tmp"):
-        file = file.split(".")
-        run("tar -xvf {}.{} -C /data/web_static/releases".format(
-            file[0], file[1]))
-        run("rm -f {}.{}".format(file[0], file[1]))
-        run("rm -f /data/web_static/current")
-        run("mv /data/web_static/releases/web_static\
-                /data/web_static/releases/{}".format(file[0]))
-        run("ln -s -f /data/web_static/releases/{}/\
-                /data/web_static/current".format(file[0]))
-    print(upload.succeeded)
+
