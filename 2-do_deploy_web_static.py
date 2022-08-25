989cd8abb6f3aa31bc91dc0108d3da53e3cce535
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-25 16:13:50 -0300

    fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -16,8 +16,6 @@ def do_pack():
     """function compress file"""
     try:
         date = datetime.utcnow().strftime('%Y%m%d%H%M%S')
-        print("Packing web_static to versions/web_static_{}.\
-                tgz web_static".format(date))
         local("sudo mkdir -p versions")
         local("sudo tar -cvzf versions/web_static_{}.tgz web_static".format(
             date))
