8f9a02c5a8125b89f7242fa82815da83e2b6622b
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-25 13:19:14 -0300

    test

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -16,6 +16,6 @@ sudo echo "<html>
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '/server_name _;/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+sudo chown -R ubuntu:ubuntu /data
+sudo sed -i '/server_name _;/a location /hbnb_static { alias /data/web_static/current;}' /etc/nginx/sites-available/default
 sudo /etc/init.d/nginx restart
