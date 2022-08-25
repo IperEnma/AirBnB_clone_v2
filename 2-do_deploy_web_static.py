41cf4a13493892cd337a396574c82b3e007d123d
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-25 17:02:07 -0300

    sdfgdsf

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """module that compress using fabric"""
 
+from fabric.operations import run, put
 from fabric.api import *
 import os
 
@@ -42,25 +43,25 @@ def do_deploy(archive_path):
     if run("mkdir -p /data/web_static/releases/{}".format(file[0])).succeeded is False:
         return False
 
-    if run("sudo tar -xzf /tmp/{}.{} -C /data/web_static/releases/{}".format(
+    if run("tar -xzf /tmp/{}.{} -C /data/web_static/releases/{}".format(
         file[0], file[1], file[0])).succeeded is False:
         return False
 
-    if run("sudo rm -rf /tmp/{}.{}".format(file[0], file[1])).succeeded is False:
+    if run("rm -rf /tmp/{}.{}".format(file[0], file[1])).succeeded is False:
         return False
 
-    if run("sudo mv /data/web_static/releases/{}/web_static/*\
+    if run("sudo cp -r /data/web_static/releases/{}/web_static/*\
             /data/web_static/releases/{}".format(file[0], file[0])).succeeded is False:
         return False
 
-    if run("sudo rm -rf /data/web_static/current").succeeded is False:
+    if run("rm -rf /data/web_static/current").succeeded is False:
         return False
 
-    if run("sudo rm -rf /data/web_static/releases/{}/web_static".format(
+    if run("rm -rf /data/web_static/releases/{}/web_static".format(
         file[0])).succeeded is False:
         return False
 
-    if run("sudo ln -s /data/web_static/releases/{}/\
+    if run("ln -s /data/web_static/releases/{}/\
             /data/web_static/current".format(file[0])).succeeded is False:
         return False
     return True
