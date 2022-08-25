ec469e9cde2767c5ffee7c6147e7edd0e631033c
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-25 13:14:23 -0300

    one check

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,14 +8,13 @@ sudo mkdir -p /data/web_static/
 sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-file="/data/web_static/releases/test/index.html"
 sudo echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" | sudo tee $file
+</html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '/server_name _;/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
