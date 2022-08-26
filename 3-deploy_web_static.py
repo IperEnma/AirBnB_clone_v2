86a0f87a0c2d95374fff7c6d7e97088ed8540437
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-25 22:08:09 -0300

    asd

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -6,18 +6,16 @@ from datetime import datetime
 from sys import argv
 import os
 
-env.hosts=["34.201.143.161", "35.175.196.152"]
-env.user=argv="ubuntu"
-env.key_filename="~/.ssh/school"
+env.hosts = ["34.201.143.161", "35.175.196.152"]
 
 
 def do_pack():
     """funtion compress file"""
     try:
         date = datetime.utcnow().strftime('%Y%m%d%H%M%S')
-        print("Packing web_static to versions/web_static_{}.tgz web_static".format(date))
         local("sudo mkdir -p versions")
-        local("sudo tar -cvzf versions/web_static_{}.tgz web_static".format(date))
+        local("sudo tar -cvzf versions/web_static_{}.tgz web_static".format(
+            date))
         return "versions/web_static_{}.tgz".format(date)
     except Exception:
         return None
@@ -60,8 +58,8 @@ def do_deploy(archive_path):
 
 def deploy():
     """function compress and send file"""
-    path=do_pack()
+    path = do_pack()
     if path is None:
         return False
-    status=do_deploy(path)
+    status = do_deploy(path)
     return status
