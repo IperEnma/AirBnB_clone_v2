798a54e04a945c3e8b188577fbc611706be1e70f
IperEnma <enmanuelhernandez1843@gmail.com>
2022-09-03 12:32:53 -0300

    requirements

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,41 +1,43 @@
 #!/usr/bin/python3
 """starts a Flask web app"""
-
-
 from flask import Flask, request
 
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def index():
+    """send string to browser"""
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes = False)
 def hbnb():
+    """send string to browser"""
     return 'HBNB'
 
 
-@app.route('/c/<text>')
+@app.route('/c/<text>', strict_slashes = False)
 def c(var):
+    """send string to browser"""
     text = text.replace("_", " ")
     return var
 
 
-@app.route('/python', defaults={'text': "is_cool"})
-@app.route('/python/<text>')
+@app.route('/python', defaults={'text': "is_cool"}, strict_slashes = False)
+@app.route('/python/<text>', strict_slashes = False)
 def python(text):
+    """send string to browser"""
     text = text.replace("_", " ")
     return "Python {}".format(text)
 
 
-@app.route('/number')
-@app.route('/number/<int:n>')
+@appd.route('/number', strict_slashes = False)
+@app.route('/number/<int:n>', strict_slashes = False)
 def number(n):
+    """send string to browser"""
     return "{} is a number".format(n)
 
-
-app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
